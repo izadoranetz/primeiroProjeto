8a589eb87b55a232dce9a2a1fd06b999d2fde1dd
Izadora Netz <izadora.ns@gmail.com>
2023-10-18 23:35:44 -0300

    testando templates

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit{
+  //nomePortal: string = 'http://andorinea.com'
+
+  nomePortal: string;
+
+  cursos: string[] = ['React', 'Angular', 'HTML', 'CSS']
+
+  constructor() {
+    this.nomePortal = 'http://andorinea.com'
+  }
+
+  ngOnInit() {
+
+  }
 
 }
